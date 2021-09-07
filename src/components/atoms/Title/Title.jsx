5c0357983e9e96a50e4e98dc6a1289a8a5a450ff
Jason Ko <jekb2019@gmail.com>
2021-09-07 21:15:49 +1200

    Fix title tag to h1

--- src/components/atoms/Title/Title.jsx
+++ src/components/atoms/Title/Title.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Title.module.css';
 
 const Title = ({ children, color, size }) => (
-  <span className={`${styles[color]} ${styles[size]}`}>{children}</span>
+  <h1 className={`${styles[color]} ${styles[size]}`}>{children}</h1>
 );
 
 export default Title;
